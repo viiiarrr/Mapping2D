6aa0b18e1eac4c571b20163f0c070a1f3bf7979a
viar15 <yoviar150405@gmail.com>
2026-04-23 15:29:13 +0700

    Cek ESP

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,15 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   int result = myFunction(2, 3);
+  Serial.println(result);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello, World!");
+  delay(1000);
 }
 
 // put function definitions here:
